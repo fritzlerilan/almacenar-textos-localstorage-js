4823c5f7c695f3785287e4d6d8bc45a0b409e31c
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-23 12:29:32 -0300

    Add validation to the form

--- js/app.js
+++ js/app.js
@@ -22,12 +22,39 @@ function agregarTweet(e) {
     e.preventDefault();
     // Text area donde el usuario escribe
     const tweet = document.querySelector('#tweet');
+
+    // Validacion
+    if ( tweet.value === '') {
+        mostrarError('Un mensaje no puede ir vacio');
+        return; // Evita que se ejecuten mas lineas de código.
+    } 
     tweets.push(tweet.value);
     // Se limpia el valor del formulario
     tweet.value = '';
     // Se sobre-escribe el LocalStorage
     localStorage.setItem('tweets', JSON.stringify(tweets));
     mostrarTweets();
+}
+
+function mostrarError(error) {
+    
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 1500);
+}
+function limpiarErrores() {
+    const errores = document.querySelectorAll('.error');
+    errores.forEach(e => {
+        e.remove();
+    })
 }
 
 function mostrarTweets() {
